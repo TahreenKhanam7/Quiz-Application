6e7d589111cf286a8aa8731e20464f1ad30be6f8
TahreenKhanam7 <tahreenkhanam7@gmail.com>
2025-08-07 21:21:50 +0530

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -4,73 +4,69 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class Rules extends JFrame implements ActionListener {
 
-public class Rules extends JFrame implements ActionListener{
-    
     String name;
-    JButton start,back;
-    
-    Rules(String name){
-        this.name= name;
+    JButton start, back;
+
+    Rules(String name) {
+        this.name = name;
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        
-        JLabel heading= new JLabel("WELCOME "+name+" to Simple Minds");
-        heading.setBounds(50,20,500,30);
-        heading.setFont(new Font("Viner Hand ITC",Font.BOLD,20));
-        heading.setForeground(new Color(30, 144, 254));
-        add (heading);
-        
-        JLabel rules= new JLabel();
-        rules.setBounds(20,110,700,400);
-        rules.setFont(new Font("Tahoma",Font.PLAIN,18));
-        rules.setText(
-            "<html>"+ 
-                "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
-                "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
-                "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
-                "4. Crying is allowed but please do so quietly." + "<br><br>" +
-                "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
-                "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
-                "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
-            "</html>"
 
+        JLabel heading = new JLabel("WELCOME " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 500, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 20));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 110, 700, 400);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        rules.setText(
+            "<html>" +
+                "1. The quiz contains 10 multiple-choice questions.<br><br>" +
+                "2. Each question has four options. Only one is correct.<br><br>" +
+                "3. You must answer each question within the time limit (e.g., 15 seconds).<br><br>" +
+                "4. No option will be selected by default. Choose carefully.<br><br>" +
+                "5. Once you move to the next question, you cannot go back.<br><br>" +
+                "6. You will get your score at the end of the quiz.<br><br>" +
+                "7. Do not refresh or close the window during the quiz.<br><br>" +
+                "8. Have fun and give your best!<br><br>" +
+            "</html>"
         );
-        add (rules);
-        
+        add(rules);
+
         back = new JButton("Back");
-        back.setBounds(250,500,100,30);
+        back.setBounds(250, 500, 100, 30);
         back.setBackground(new Color(30, 144, 254));
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
-        add (back);
-        
+        add(back);
+
         start = new JButton("Start");
-        start.setBounds(400,500,100,30);
+        start.setBounds(400, 500, 100, 30);
         start.setBackground(new Color(30, 144, 254));
         start.setForeground(Color.WHITE);
         start.addActionListener(this);
-        add (start);
-        
-        
-        
-        setSize(720,580);
+        add(start);
+
+        setSize(720, 580);
         setLocation(300, 100);
         setVisible(true);
     }
-    
-    public void  actionPerformed(ActionEvent ae){
-        if (ae.getSource()== start){
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
             setVisible(false);
             new Quiz(name);
-        }else {
+        } else {
             setVisible(false);
             new Login();
         }
     }
+
     public static void main(String[] args) {
         new Rules("USER");
     }
-    
 }
